9e5b1ca70ad3c1309f00ec69d3def3138898b401
Kalin Rudnicki <kalin.rudnicki@gmail.com>
2022-04-09 12:03:01 -0500

    Add config

--- slyce-generate/src/main/scala/slyce/generate/Main.scala
+++ slyce-generate/src/main/scala/slyce/generate/Main.scala
@@ -8,12 +8,48 @@ object Main extends ExecutableApp {
 
   private object generate {
 
+    final case class SingleConfig(
+        lexerFile: String,
+        grammarFile: String,
+        outputFile: String,
+        targetLanguage: Option[TargetLanguage],
+    )
+    object SingleConfig {
+
+      val parser: Parser[SingleConfig] = {
+        Parser.singleValue[String]("lexer-file").required >&>
+          Parser.singleValue[String]("grammar-file").required >&>
+          Parser.singleValue[String]("output-file").required >&>
+          Parser.singleValue.enumValues("target-language", TargetLanguage.values).optional
+      }.map(SingleConfig.apply)
+
+    }
+
+    private def fileFromPath(path: String): STaskM[File] =
+      File.fromPath(path).flatMap { file =>
+        file.ensureExists *>
+          file.isFile.flatMap {
+            case true  => ZIO.succeed(file)
+            case false => ZIO.fail(KError.message.same(s"Not a file: $path"))
+          }
+      }
+
     private val single: Executable =
       Executable
-        .fromParser(Parser.unit.disallowExtras)
+        .fromParser(SingleConfig.parser.disallowExtras)
         .withLayer(_ => ZIO.unit.toLayer)
-        .withExecute { _ =>
-          Logger.println.info("Running generate/single")
+        .withExecute { config =>
+          for {
+            _ <- Logger.println.info("Running generate/single")
+            lexerFile <- fileFromPath(config.lexerFile)
+            grammarFile <- fileFromPath(config.grammarFile)
+            outputFile <- File.fromPath(config.outputFile)
+            targetLanguage <-
+              ZIO
+                .fromOption(TargetLanguage.parse(config.targetLanguage, outputFile.fileName.ext))
+                .orElseFail(KError.message.same("Unable to assume target language"))
+            _ <- Logger.println.info(targetLanguage)
+          } yield ()
         }
 
     val executable: Executable =
---
+++ slyce-generate/src/main/scala/slyce/generate/TargetLanguage.scala
@@ -0,0 +1,18 @@
+package slyce.generate
+
+import cats.syntax.option.*
+
+enum TargetLanguage {
+  case Scala3
+}
+object TargetLanguage {
+
+  def parse(targetLanguage: Option[TargetLanguage], extName: Option[String]): Option[TargetLanguage] =
+    targetLanguage.orElse {
+      extName match {
+        case Some("scala") => TargetLanguage.Scala3.some
+        case _             => None
+      }
+    }
+
+}
