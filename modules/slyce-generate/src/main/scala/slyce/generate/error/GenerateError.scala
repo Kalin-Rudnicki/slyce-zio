1ccb09d9bf29023b84de5cb96cf6dedcbdf64d25
Kalin-Rudnicki <kalin.r.rudnicki@gmail.com>
2024-03-17 12:43:53 -0600

    Filled in GenerateError.getMessage

--- modules/slyce-generate/src/main/scala/slyce/generate/error/GenerateError.scala
+++ modules/slyce-generate/src/main/scala/slyce/generate/error/GenerateError.scala
@@ -1,10 +1,19 @@
 package slyce.generate.error
 
 import cats.data.NonEmptyList
+import harness.core.*
 
-import slyce.core.Marked
+import slyce.core.*
 
-sealed trait GenerateError extends Throwable
+sealed trait GenerateError extends Throwable {
+
+  override final def getMessage: String = this match {
+    case GenerateError.FailedValidation(errors) => Source.markAll(errors.toList)
+    case GenerateError.InvalidInput(message)    => message
+    case GenerateError.Unexpected(error)        => s"Unexpected error: ${error.safeGetMessage}"
+  }
+
+}
 object GenerateError {
 
   final case class FailedValidation(errors: NonEmptyList[Marked[String]]) extends GenerateError
